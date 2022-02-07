e6372cb97fc565081ae53d27dd02e387e7178a2a
esn73 <esn7338@gmail.com>
2022-02-07 12:20:36 +0900

    .

--- Spring02/src/main/java/edu/spring/ex02/persistence/ReplyDaoImpl.java
+++ Spring02/src/main/java/edu/spring/ex02/persistence/ReplyDaoImpl.java
@@ -1,5 +1,7 @@
 package edu.spring.ex02.persistence;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,6 +24,28 @@ public class ReplyDaoImpl implements ReplyDao{
 		log.info("create({}) 호출", reply);
 		
 		return sqlSession.insert(NAMESPACE + ".insert", reply);
+	}
+
+	@Override
+	public List<Reply> read(int bno) {
+		log.info("read(bno={} 호출)", bno);
+		
+		return sqlSession.selectList(NAMESPACE + ".select", bno);
+	}
+
+	@Override
+	public int update(Reply reply) {
+		log.info("update(bno={} 호출)", reply);
+		
+		return sqlSession.update(NAMESPACE + ".update", reply);
+	}
+
+	@Override
+	public int delete(int rno) {
+		log.info("delete(rno={}) 호출", rno);
+		
+		return sqlSession.delete(NAMESPACE + ".delete", rno);
+		
 	}
 
 }
--- Spring02/src/test/java/edu/spring/ex02/ReplyDaoTest.java
+++ Spring02/src/test/java/edu/spring/ex02/ReplyDaoTest.java
@@ -1,5 +1,7 @@
 package edu.spring.ex02;
 
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -28,9 +30,25 @@ public class ReplyDaoTest {
 	public void doTest() {
 		log.info("replyDao: {}", replyDao);
 		
-		Reply reply = new Reply(0, "DAO 테스트", "admin", null, 1);
+		/*
+		Reply reply = new Reply(0, "DAO 테스트 2", "admin", null, 2);
 		int result = replyDao.create(reply);
 		log.info("insert result:{}", result);
+		*/
+		
+		/*
+		List<Reply> replyList = replyDao.read(2);
+		log.info(replyList.toString());
+		*/
+		
+		/*
+		Reply reply = new Reply(1, "댓글 수정 테스트", null, null, 0);
+		int result  = replyDao.update(reply);
+		log.info("update result: {}", result);
+		*/
+		
+		int result = replyDao.delete(1);
+		log.info("delete result: {}", result);
 	}
 	
 }
